5dfdb995608a884758b82e26e4e9e86b6173b77b
Prasad Sawant <prasadsawant323@gmail.com>
2026-06-29 20:53:09 +0530

    Reposition decorative terminal icon to avoid text overlap

--- src/components/JourneySpace.tsx
+++ src/components/JourneySpace.tsx
@@ -99,7 +99,7 @@ export default function JourneySpace() {
         {/* Core Project Panel */}
         {activeMilestone.projectTitle && (
         <div className="bg-gradient-to-br from-zinc-950/80 to-zinc-900/60 border border-white/10 rounded-2xl p-5 space-y-3.5 relative">
-          <div className="absolute top-0 -right-6 p-3 opacity-15">
+          <div className="absolute top-0 -right-4 p-3 opacity-15">
             <Terminal className="w-20 h-20 text-white" />
           </div>
 
